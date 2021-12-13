13070977fcac6cd6925dd558e28838ad2c85d554
saeb <saeb>
2021-12-13 13:50:11 +0200

    done

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff00b8d4);
 const kPrimaryLightColor2 = Color(0xff18ffff);
 const kPrimaryLightColor = Color(0xffffffff);
+
+const kPrimaryLightColor3 = Color(0xffffffff);
